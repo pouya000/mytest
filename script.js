c4776d07e82d0f4164bd326f6a60caff177fe98b
puya <puya.shamsi@gmail.com>
2024-07-16 20:43:08 +0330

    change line 3 in script.html

---
+++ script.js
@@ -0,0 +1,35 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const inputInvalid = document.getElementById('input-invalid');
+const itemList = document.getElementById('item-list');
+
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', onClickItem);
+
+function addItem(e) {
+    const newItem = itemInput.value;
+    e.preventDefault();
+
+    // Validate Input start
+    if (newItem == '') {
+        inputInvalid.innerText = 'لطفا متن مورد نظر را وارد کنید';
+        return;
+    } 
+    // Validate Input end
+
+    const li = document.createElement('li');
+    li.textContent = newItem;
+    li.className = 'list-item';
+    const icon = document.createElement('i');
+    icon.className = 'fa fa-user-times';
+    li.appendChild(icon);
+    itemList.appendChild(li);
+    itemInput.value = '';
+}
+
+function onClickItem(e) {
+    e.target.parentElement.remove();   
+}
+
+// event listeners ------
+
